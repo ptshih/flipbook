0cc618998f85e920646ff6240ff1a78026f84fad
Peter Shih <peter@foursquare.com>
2012-02-28 13:12:46 -0500

    asset cleanup

--- Classes/TimelineViewController.m
+++ Classes/TimelineViewController.m
@@ -62,7 +62,7 @@ shouldRefreshOnAppear = _shouldRefreshOnAppear;
 }
 
 //- (UIView *)baseBackgroundView {
-//  UIImageView *bgView = [[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"BackgroundWeave.jpg"]] autorelease];
+//  UIImageView *bgView = [[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"BackgroundLeather.jpg"]] autorelease];
 //  return bgView;
 //}
 
